15580a8de965498496b6f6e187ea3a9366676a56
Ashar581 <101134992+Ashar581@users.noreply.github.com>
2023-12-02 05:04:17 +0530

    Update DemoController.java

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DemoController {
 
-    @RequestMapping("/")
+    @RequestMapping("/hi")
     public String checkJsp(){
         return "ashar";
     }
