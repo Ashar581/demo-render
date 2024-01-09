314f3f9fbc6a2f7343c86726645d6c269cd67292
Ashar581 <asharshahab@gmail.com>
2024-01-10 01:32:19 +0530

    git push

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,18 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@RequestMapping("index")
+	@RequestMapping("/index")
 	public String checkJsp(){
-		return "Hello Ashar Shahab!";
+		return "index";
 	}
 }
 
